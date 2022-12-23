3a2c258cfe557da53404b8ebb1d583115d983b24
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2022-12-23 19:59:06 +0530

    Add files via upload

---
+++ .github/workflows/module2.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+model=tf.keras.models.Sequential([])
+
+#  1 CNN layer
+  tf.keras.layers.Conv2D(64,(3,3),activation="relu",input_shape(180,180,3))
+  model.summary()
+# 2 CNN Layer
+  tf.keras.layers.Conv2D(64,(3,3),activation="relu",input_shape(180,180,3))
+   model.summary()
+# 3 CNN layer
+  tf.keras.layers.Conv2D(64,(3,3),activation="relu",input_shape(180,180,3))
+   model.summary()
+# 4 CNN Layer
+  tf.keras.layers.Conv2D(64,(3,3),activation="relu",input_shape(180,180,3))
+   model.summary()
+
